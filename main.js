7f35e50b0e23605af3f09060bfaa1ed0ad560206
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-04-02 19:46:39 +0000

    Sample code

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let listTask = document.forms['list__task'];
 let addTask = document.forms['add__task']
 let modal = document.querySelector('.modal')
-let btnAdd = document.getElementById('btnAdd');
+let btnAdd = document.querySelector('.btnAdd');
 let close = document.querySelector('.close')
 let text = document.getElementById('text');
-let list = document.getElementById('list');
+let list = document.querySelector('list');
 let dd = document.getElementsByTagName('label')
+let checkbox = document.querySelector('.checkbox')
 
 btnAdd.addEventListener('click', function () {
     if(text.value != '') {
@@ -23,32 +24,17 @@ btnAdd.addEventListener('click', function () {
         listTask.appendChild(label);
         text.value = ''
     } else {
-        // modal window
-        addTask.classList.add('.modal');
-        modal.style.display = 'block';
         addTask.addEventListener('click', function() {
-            modal.style.display = "none";
+            alert('It is not possible to add an empty field')
         })
     }
     for(let i = 0; i < listTask.children.length; i++) {
     listTask.children[i].addEventListener('click', function() {
-        if (listTask.children.length <= 1) {
-            alert('Останній елемент неможливо видалити')
-        }
-        if(listTask) {
+        if (listTask.children.length < 1) {
+            alert('The last element cannot be deleted')
+        } else {
             this.remove()
         }
     })
 }
 })
-
-
-
-
-
-
-
-
-
-
-  
